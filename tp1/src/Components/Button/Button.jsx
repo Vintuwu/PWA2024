80607666ff9621f879b25f07a8cafbf2fa1a1cb0
Agustin Chiarotto <agusgc_99@hotmail.com>
2024-04-28 21:30:04 -0300

    correcciones

--- tp1/src/Components/Button/Button.jsx
+++ tp1/src/Components/Button/Button.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 
 export default function Button({ onClick, icon, tipo, classNameProp }) {
-    return (
-      <button onClick={onClick} className={`${classNameProp} btn btn-${tipo}`}>
-        {icon}
-      </button>
-    );
-  }
-  
+  return (
+    <button onClick={onClick} className={`${classNameProp} btn btn-${tipo}`}>
+      {icon}
+    </button>
+  );
+}
--- tp1/src/Components/Input/Input.jsx
+++ tp1/src/Components/Input/Input.jsx
@@ -1,13 +1,22 @@
-import style from "./Input.module.css";
-const Input = ({ tipo, value, placeholder, onChangeHandler, onKeyDown, classNameProp}) => {
-    return <input
-                className={`${classNameProp}`}
-                type={tipo}
-                value={value}
-                placeholder={placeholder}
-                onChange={onChangeHandler}
-                onKeyDown={onKeyDown}
-            />;
-  };
-  
+import style from "./Input.module.css"; //No se esta utilizando
+const Input = ({
+  tipo,
+  value,
+  placeholder,
+  onChangeHandler,
+  onKeyDown,
+  classNameProp,
+}) => {
+  return (
+    <input
+      className={`${classNameProp}`}
+      type={tipo}
+      value={value}
+      placeholder={placeholder}
+      onChange={onChangeHandler}
+      onKeyDown={onKeyDown}
+    />
+  );
+};
+
 export default Input;
