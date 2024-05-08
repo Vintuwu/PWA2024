067f6a701309a25599a50c377a83bbc69dfa0d6a
Emiliano Matias Lopez <emiliano.lopez@est.fi.uncoma.edu.ar>
2024-05-08 17:30:35 -0300

    Funcionalidad de la SearchBar, uselocation para SearchBar  y leer desc
    También pase unos estilos con apply de Footer.jsx a Footer.module.css

--- tp2/src/App.jsx
+++ tp2/src/App.jsx
@@ -1,21 +1,28 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import Details from "./pages/Details/Details";
-import Header from "./Components/Header/Header";
-import Footer from "./Components/Footer/Footer";
-import "./App.css";
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Details from './pages/Details/Details';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import './App.css';
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleInputChange = (term) => {
+    setSearchTerm(term);
+  };
+
   return (
     <>
-    <Header/>
-      <Router>
+    <Router>
+      <Header onInputChange={handleInputChange} />
         <Routes>
-          <Route exact path="/" element={<Home />} />
+          <Route exact path="/" element={<Home searchTerm={searchTerm} />} />
           <Route exact path="/details/:id" element={<Details />} />
         </Routes>
+      <Footer />
       </Router>
-      <Footer/>
     </>
   );
 }
