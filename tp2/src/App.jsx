a60a0154b4a15243775523fb7e3743117e9195ab
Emiliano Matias Lopez <emiliano.lopez@est.fi.uncoma.edu.ar>
2024-05-08 02:14:16 -0300

    Header y footer Dinamicos (? y Correccion de clases

--- tp2/src/App.jsx
+++ tp2/src/App.jsx
@@ -1,18 +1,21 @@
-
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Details from "./pages/Details/Details";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
 import "./App.css";
 
 function App() {
   return (
     <>
+    <Header/>
       <Router>
         <Routes>
-          <Route exact path="/" element={<Home />}  />
-          <Route exact path="/details/:id" element={<Details />}  />
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/details/:id" element={<Details />} />
         </Routes>
       </Router>
+      <Footer/>
     </>
   );
 }
--- tp2/src/pages/Home/Home.jsx
+++ tp2/src/pages/Home/Home.jsx
@@ -1,12 +1,9 @@
-import Header from '../../Components/Header/Header';
-import Footer from '../../Components/Footer/Footer';
+
 import CardList from '../../Components/CardList/CardList';
 const Home = () => {
     return (
         <div>
-            <Header></Header>
             <CardList/>
-            <Footer></Footer>
         </div>
     )
 }
