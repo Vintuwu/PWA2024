764c175c779499b7981d5d64b2f014291bc35c98
Emiliano Matias Lopez <emiliano.lopez@est.fi.uncoma.edu.ar>
2024-05-08 20:42:05 -0300

    Peliculas no encontradas

--- tp2/src/Components/CardList/CardList.jsx
+++ tp2/src/Components/CardList/CardList.jsx
@@ -1,22 +1,22 @@
-import Card from '../Card/Card';
+import Card from "../Card/Card";
 import style from "./CardList.module.css";
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 // Componente CardList que muestra la lista de películas guardadas en el json
-const CardList = ( {searchTerm} ) => {
+const CardList = ({ searchTerm }) => {
   const [movies, setMovies] = useState([]);
   const fetchMovies = async () => {
     try {
       const response = await fetch(`../src/mocks/movies.json`);
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error("Network response was not ok");
       }
       const result = await response.json();
       setMovies(result);
     } catch (error) {
-      console.error('Error fetching movies:', error);
+      console.error("Error fetching movies:", error);
     }
-  }
+  };
   useEffect(() => {
     fetchMovies();
   });
@@ -24,7 +24,6 @@ const CardList = ( {searchTerm} ) => {
   var filteredMovies = Array();
   // Chequeo que el termino de busqueda no sea indefinido
   if (searchTerm != undefined) {
-  
     movies.forEach((movie) => {
       // filtro las peliculas que cuyo titulo coincida con el termino de busqueda
       if (movie.Title.toLowerCase().includes(searchTerm.toLowerCase())) {
@@ -35,6 +34,34 @@ const CardList = ( {searchTerm} ) => {
   } else {
     // Si no hay termino de busqueda no se hace nada
     filteredMovies = movies;
+  }
+
+  if (filteredMovies.length === 0) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="flex flex-col items-center justify-center text-lg">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-9 h-9"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z"
+            />
+          </svg>
+
+          <p>
+            No hay peliculas que coincidan con el titulo "
+            <strong>{searchTerm}</strong>"
+          </p>
+        </div>
+      </div>
+    );
   }
   return (
     <div className={style.card_list}>
